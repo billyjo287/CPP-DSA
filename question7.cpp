54942c6b4c0d1a270da64ab22bed3494594e2f32
JoRutozi <billy.igiraneza@strathmore.edu>
2026-04-21 14:55:55 +0300

    Initial commit

--- question7.cpp
+++ question7.cpp
@@ -1,19 +1,58 @@
-//
-// Created by billy on 4/21/2026.
-//
-
 #include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
+int absoluteDistinctCount(const vector<int>& A) {
+    int left = 0;
+    int right = A.size() - 1;
+
+    int count = 0;
+    int last = -1; // track last counted absolute value
+
+    while (left <= right) {
+        int leftAbs = abs(A[left]);
+        int rightAbs = abs(A[right]);
+
+        int current;
+
+        if (leftAbs > rightAbs) {
+            current = leftAbs;
+            left++;
+        } else if (leftAbs < rightAbs) {
+            current = rightAbs;
+            right--;
+        } else {
+            current = leftAbs;
+            left++;
+            right--;
+        }
+
+        if (current != last) {
+            count++;
+            last = current;
+        }
+    }
+
+    return count;
+}
+
 int main() {
-    int days;
+    int n;
 
-    cout << "Enter number of days: ";
-    cin >> days;
+    cout << "Enter number of elements: ";
+    cin >> n;
 
-    long long seconds = days * 24LL * 60 * 60;
+    vector<int> A(n);
 
-    cout << "Seconds in " << days << " days is: " << seconds << endl;
+    cout << "Enter sorted elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+
+    int result = absoluteDistinctCount(A);
+
+    cout << "Absolute distinct count: " << result << endl;
 
     return 0;
 }
